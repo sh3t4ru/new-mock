70faea1ef4666b65c23b2c525dd00fe6f022c2ea
sh3t4ru <sh3t4ru@gmail.com>
2024-07-13 03:08:12 +0600

    Added kafka producing, version fails to start

--- src/main/java/com/example/new_mock/controller/MainController.java
+++ src/main/java/com/example/new_mock/controller/MainController.java
@@ -4,9 +4,12 @@ import com.example.new_mock.model.RequestDTO;
 import com.example.new_mock.model.ResponseDTO;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,13 @@ import java.util.Random;
 @Slf4j
 @RestController
 public class MainController {
-    private ObjectMapper mapper = new ObjectMapper();
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @Value(value = "${spring.kafka.topic}")
+    private String topic;
 
     @PostMapping(
             value = "/info/postBalances",
@@ -48,14 +57,25 @@ public class MainController {
             responseDTO.setBalance(randomBalance);
             responseDTO.setMaxLimit(maxLimit);
 
-            log.error("********** RequestDTO **********" +
+            log.debug("********** RequestDTO **********" +
                     mapper.writerWithDefaultPrettyPrinter().writeValueAsString(requestDTO));
-            log.error("********** ResponseDTO **********" +
+            log.debug("********** ResponseDTO **********" +
                     mapper.writerWithDefaultPrettyPrinter().writeValueAsString(responseDTO));
+
+            // Kafka produce
+            kafkaTemplate.send(topic, mapper.writeValueAsString(responseDTO))
+                    .whenComplete((result, ex) -> {
+                        if (ex == null) {
+                            log.info(result.toString());
+                        } else {
+                            log.error(ex.getMessage());
+                        }
+                    });
 
             return responseDTO;
 
         } catch (Exception e) {
+            log.warn(e.getMessage());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
     }
